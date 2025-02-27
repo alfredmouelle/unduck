a2dbcec84953657381bffc7fab271efed952fd57
Alfred MOUELLE <alfredmouelle@gmail.com>
2025-02-27 21:26:30 +0100

    feat: setup default bang

--- src/unduck.tsx
+++ src/unduck.tsx
@@ -1,8 +1,12 @@
-import { Action, ActionPanel, Form, open } from "@raycast/api";
+import { Action, ActionPanel, Form, getPreferenceValues, open } from "@raycast/api";
 
 export default function Command() {
+  const preferences = getPreferenceValues<{ defaultBang: string }>();
+  const defaultBang = preferences.defaultBang;
+
   const handleSubmit = (values: { query: string }) => {
-    open(`https://unduck.link?q=${encodeURIComponent(values.query)}`);
+    const query = /!\w+/.test(values.query) ? values.query : `${values.query} ${defaultBang}`;
+    open(`https://unduck.link?q=${encodeURIComponent(query)}`);
   };
 
   return (
@@ -15,6 +19,7 @@ export default function Command() {
     >
       <Form.Description text="Search anything, everywhere, using thousands of available bangs 💥" />
       <Form.TextArea id="query" placeholder="alfredmouelle !ghrepo" />
+      <Form.Description text="Credit : Theo Browne (t3dotgg)" />
     </Form>
   );
 }
