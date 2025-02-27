c50426efaa2dbdc210b81c3de491f7cdd0bb68b8
Alfred MOUELLE <alfredmouelle@gmail.com>
2025-02-27 22:18:58 +0100

    feat: allow save searches

--- src/unduck.tsx
+++ src/unduck.tsx
@@ -1,12 +1,29 @@
-import { Action, ActionPanel, Form, getPreferenceValues, open } from "@raycast/api";
+import { Action, ActionPanel, Form, getPreferenceValues, LocalStorage, open } from "@raycast/api";
+import { useEffect, useState } from "react";
 
 export default function Command() {
-  const preferences = getPreferenceValues<{ defaultBang: string }>();
-  const defaultBang = preferences.defaultBang;
+  const [savedSearches, setSavedSearches] = useState<string[]>([]);
+  const preferences = getPreferenceValues<{ defaultBang: string; saveSearch: boolean }>();
 
-  const handleSubmit = (values: { query: string }) => {
-    const query = /!\w+/.test(values.query) ? values.query : `${values.query} ${defaultBang}`;
-    open(`https://unduck.link?q=${encodeURIComponent(query)}`);
+  useEffect(() => {
+    const loadSearches = async () => {
+      const savedSearches = await LocalStorage.getItem<string>("searches");
+      if (savedSearches) setSavedSearches(JSON.parse(savedSearches));
+    };
+    loadSearches();
+  }, []);
+
+  const handleSubmit = async (values: { query: string; saveSearch: boolean; savedSearches: string }) => {
+    let searchQuery = values.savedSearches;
+    if (!searchQuery.length) {
+      searchQuery = /!\w+/.test(values.query) ? values.query : `${values.query} ${preferences.defaultBang}`;
+    }
+    open(`https://unduck.link?q=${encodeURIComponent(searchQuery)}`);
+    if (values.saveSearch && values.query && !savedSearches.includes(values.query)) {
+      const newSavedSearches = [...savedSearches, values.query];
+      setSavedSearches(newSavedSearches);
+      await LocalStorage.setItem("searches", JSON.stringify(newSavedSearches));
+    }
   };
 
   return (
@@ -17,8 +34,20 @@ export default function Command() {
         </ActionPanel>
       }
     >
-      <Form.Description text="Search anything, everywhere, using thousands of available bangs 💥" />
       <Form.TextArea id="query" placeholder="alfredmouelle !ghrepo" />
+
+      <Form.Checkbox id="saveSearch" label="Save Search" defaultValue={preferences.saveSearch} />
+
+      <Form.Dropdown id="savedSearches" title="Saved Searches">
+        <Form.Dropdown.Item key="0" value="" title="No saved searches" />
+
+        {savedSearches.map((search, index) => (
+          <Form.Dropdown.Item key={index + 1} value={search} title={search} />
+        ))}
+      </Form.Dropdown>
+
+      <Form.Separator />
+
       <Form.Description text="Credit : Theo Browne (t3dotgg)" />
     </Form>
   );
